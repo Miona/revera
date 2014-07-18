c3e00731859be0d3b7bc216a909f92534009843c
Miona <shruti.taldar@gmail.com>
2014-07-18 11:17:01 +0530

    commit

--- includes/custom-header.php
+++ includes/custom-header.php
@@ -13,41 +13,41 @@
 	<?php } // if ( ! empty( $header_image ) ) ?>
 
  *
- * @package _tk
+ * @package Revera
  */
 
 /**
  * Setup the WordPress core custom header feature.
  *
- * @uses _tk_header_style()
- * @uses _tk_admin_header_style()
- * @uses _tk_admin_header_image()
+ * @uses revera_header_style()
+ * @uses revera_admin_header_style()
+ * @uses revera_admin_header_image()
  *
- * @package _tk
+ * @package Revera
  */
-function _tk_custom_header_setup() {
+function revera_custom_header_setup() {
 	if ( function_exists( 'add_theme_support' ) ) {
-		add_theme_support( 'custom-header', apply_filters( '_tk_custom_header_args', array(
+		add_theme_support( 'custom-header', apply_filters( 'revera_custom_header_args', array(
 			'default-image'          => '',
 			'default-text-color'     => '000',
 			'width'                  => 1170,
 			'height'                 => 250,
 			'flex-height'            => true,
-			'wp-head-callback'       => '_tk_header_style',
-			'admin-head-callback'    => '_tk_admin_header_style',
-			'admin-preview-callback' => '_tk_admin_header_image',
+			'wp-head-callback'       => 'revera_header_style',
+			'admin-head-callback'    => 'revera_admin_header_style',
+			'admin-preview-callback' => 'revera_admin_header_image',
 		) ) );
 	}
 }
-add_action( 'after_setup_theme', '_tk_custom_header_setup' );
+add_action( 'after_setup_theme', 'revera_custom_header_setup' );
 
-if ( ! function_exists( '_tk_header_style' ) ) :
+if ( ! function_exists( 'revera_header_style' ) ) :
 /**
  * Styles the header image and text displayed on the blog
  *
- * @see _tk_custom_header_setup().
+ * @see revera_custom_header_setup().
  */
-function _tk_header_style() {
+function revera_header_style() {
 	$header_text_color = get_header_textcolor();
 
 	// If no custom options for text are set, let's bail
@@ -79,15 +79,15 @@ function _tk_header_style() {
 	</style>
 	<?php
 }
-endif; // _tk_header_style
+endif; // revera_header_style
 
-if ( ! function_exists( '_tk_admin_header_style' ) ) :
+if ( ! function_exists( 'revera_admin_header_style' ) ) :
 /**
  * Styles the header image displayed on the Appearance > Header admin panel.
  *
- * @see _tk_custom_header_setup().
+ * @see revera_custom_header_setup().
  */
-function _tk_admin_header_style() {
+function revera_admin_header_style() {
 ?>
 	<style type="text/css">
 		.appearance_page_custom-header #headimg {
@@ -107,15 +107,15 @@ function _tk_admin_header_style() {
 	</style>
 <?php
 }
-endif; // _tk_admin_header_style
+endif; // revera_admin_header_style
 
-if ( ! function_exists( '_tk_admin_header_image' ) ) :
+if ( ! function_exists( 'revera_admin_header_image' ) ) :
 /**
  * Custom header image markup displayed on the Appearance > Header admin panel.
  *
- * @see _tk_custom_header_setup().
+ * @see revera_custom_header_setup().
  */
-function _tk_admin_header_image() {
+function revera_admin_header_image() {
 	$style        = sprintf( ' style="color:#%s;"', get_header_textcolor() );
 	$header_image = get_header_image();
 ?>
@@ -128,4 +128,4 @@ function _tk_admin_header_image() {
 	</div>
 <?php
 }
-endif; // _tk_admin_header_image
+endif; // revera_admin_header_image
